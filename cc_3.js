b237d3a999cdd963f74e2200d299597d92720df1
Jordan Higgins <jordanhiggins@usf.edu>
2025-01-31 13:24:25 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -6,3 +6,10 @@ prices.shift();
 console.log("New Prices:" ,prices); 
 // Declares prices to the array, added 200 to the end of the array, and removed the first price from the array. Then, prints the code in the console.
 
+// Task 2: Modifying Customer Orders
+let orders = [25, 300, 125, 175, 100]
+orders[2] += 5;
+let ordersTotal = orders.reduce((total, orders) => total + orders, 0);
+
+console.log("Total Orders: ", ordersTotal);
+// Declares orders to the array, added 5 to the 3rd number in the array, calculated the total orders, then had the console print the new order information.
