705eeebd4e588c96893d78e1d6ae4b00812a0ce9
Jordan Higgins <jordanhiggins@usf.edu>
2025-01-31 12:54:43 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,8 @@
+// Task 1: Product Price Management
+let prices = [25, 50, 75, 100, 150];
+prices.push(200);
+prices.shift();
+
+console.log("New Prices:" ,prices); 
+// Declares prices to the array, added 200 to the end of the array, and removed the first price from the array. Then, prints the code in the console.
+
